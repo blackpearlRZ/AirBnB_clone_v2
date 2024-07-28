e8fbb0a2e4c4f02673122c8262db9f3670371167
chaimae <chaimaeaafif@gmail.com>
2024-07-28 14:02:12 -0700

    model

--- models/base_model.py
+++ models/base_model.py
@@ -2,36 +2,53 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from os import environ
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    __abstract__ = True
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+            self.id = kwargs.get("id", str(uuid.uuid4()))
+            self.created_at = kwargs.get("created_at", datetime.now())
+            self.updated_at = kwargs.get("updated_at", datetime.now())
 
-    def __str__(self):
+    def __repr__(self):
         """Returns a string representation of the instance"""
+        str_rep = self.__dict__
+        try:
+            del(str_rep["_sa_instance_state"])
+        except KeyError:
+            pass
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, str_rep)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.__dict__["updated_at"] = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        from models import storage
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -39,6 +56,13 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        try:
+            dictionary['created_at'] = self.created_at.isoformat()
+            dictionary['updated_at'] = self.updated_at.isoformat()
+        except Exception:
+            pass
+        try:
+            del(dictionary["_sa_instance_state"])
+        except Exception:
+            pass
         return dictionary
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import environ
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade="all, delete-orphan")
+
+    if not environ.get('HBNB_TYPE_STORAGE') == "db":
+        @property
+        def cities(self):
+            from models import storage
+            cities_list = [city for _, city in storage.all("City").items()
+                           if city.state_id == self.id]
+            return cities_list
